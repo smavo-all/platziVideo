1111a3836205730331149e111ed11c3b8ff34eb6
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-08-04 22:47:24 -0500

    add Header.jsx, Header.scss

--- src/Index.js
+++ src/Index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 // import HelloWorld from './components/HelloWorld';
-//import Header from './components/Header';
+// import Header from './components/Header';
 import Header from './components/Header';
 
 ReactDom.render(<Header />, document.getElementById('app'));
