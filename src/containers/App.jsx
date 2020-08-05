76a57c3e7102650fd80d4eb74dacbc3e5d88d00c
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-08-05 00:09:52 -0500

    add Footer [.jsx,scss]

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,20 +4,27 @@ import Search from '../components/Search';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Categories from '../components/Categories';
+import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => (
   <div className='App'>
     <Header />
     <Search />
+
     <Categories>
       <Carousel>
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
       </Carousel>
     </Categories>
+
+    <Footer />
   </div>
 );
 
