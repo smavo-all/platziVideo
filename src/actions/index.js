abe7f2f15f52f650ae2fad276d0bbe203aa3955d
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-08-07 11:25:45 -0500

    add login, logout

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,19 @@
 export const setFavorite = payload => ({
-    type: 'SET_FAVORITE',
-    payload,
+  type: 'SET_FAVORITE',
+  payload,
 });
 
 export const deleteFavorite = payload => ({
-    type: 'DELETE_FAVORITE',
-    payload,
+  type: 'DELETE_FAVORITE',
+  payload,
 });
 
 export const loginRequest = payload => ({
-    type:  'LOGIN_REQUEST',
-    payload
+  type: 'LOGIN_REQUEST',
+  payload,
+});
+
+export const logoutRequest = payload => ({
+  type: 'LOGOUT_REQUEST',
+  payload,
 });
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,6 +1,6 @@
-import React , {useState} from 'react';
-import { connect} from 'react-redux';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { loginRequest } from '../actions';
 import '../assets/styles/components/Login.scss';
 import googleIcon from '../assets/static/google-icon.png';
@@ -8,63 +8,79 @@ import twitterIcon from '../assets/static/twitter-icon.png';
 
 //Componente presentacional
 const Login = (props) => {
-  const [form, setValues] = useState ({
+  const [form, setValues] = useState({
     email: '',
   });
 
-const handleInput = event => {
-  setValues ({
-    ...form,
-    [event.target.name]: event.target.value
-  })
-};
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  props.loginRequest(form);
-  props.history.push('/');
-}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.loginRequest(form);
+    props.history.push('/');
+  };
 
   return (
-  <section className='login'>
-    <section className='login__container'>
-      <h2>Inicia sesión</h2>
-      <form className='login__container--form' onSubmit={handleSubmit}>
-        <input  name="email"
-                className="input" type="text"
-                placeholder="Correo"
-                onChange={handleInput}
-        />
-        <input  name="password"
-                className="input" type="password"
-                placeholder="Contraseña"
-                onChange={handleInput}
-        />
-        <button className='button'>Iniciar sesión</button>
-        <div className='login__container--remember-me'>
-          <label>
-            <input type='checkbox' id='cbox1'
-                    value='first_checkbox' /> Recuérdame
-              </label>
-          <a href='/'>Olvidé mi contraseña</a>
-        </div>
-      </form>
-      <section className='login__container--social-media'>
-        <div><img src={googleIcon} /> Inicia sesión con Google</div>
-        <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
+    <section className='login'>
+      <section className='login__container'>
+        <h2>Inicia sesión</h2>
+        <form className='login__container--form' onSubmit={handleSubmit}>
+          <input
+            name='email'
+            className='input'
+            type='text'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            className='input'
+            type='password'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button className='button'>Iniciar sesión</button>
+          <div className='login__container--remember-me'>
+            <label>
+              <input
+                type='checkbox'
+                id='cbox1'
+                value='first_checkbox'
+              />
+              {' '}
+              Recuérdame
+            </label>
+            <a href='/'>Olvidé mi contraseña</a>
+          </div>
+        </form>
+        <section className='login__container--social-media'>
+          <div>
+            <img src={googleIcon} />
+            Inicia sesión con Google
+          </div>
+          <div>
+            <img src={twitterIcon} />
+            Inicia sesión con Twitter
+          </div>
+        </section>
+        <p className='login__container--register'>
+          No tienes ninguna cuenta
+          <Link to='/registro'>
+            Regístrate
+          </Link>
+        </p>
       </section>
-      <p className='login__container--register'>
-        No tienes ninguna cuenta {' '}
-        <Link to='/registro'>
-          Regístrate
-        </Link>
-      </p>
     </section>
-  </section>
-)};
+  );
+};
 
-const mapDispatchToProps ={
+const mapDispatchToProps = {
   loginRequest,
 };
 
-export default connect(null, mapDispatchToProps) (Login);
+export default connect(null, mapDispatchToProps)(Login);
