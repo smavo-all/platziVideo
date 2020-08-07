6024a55f378bf7639a88f212fdb15342f24eca08
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-08-06 21:05:43 -0500

    add componente Layout, NotFound

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Categories from '../components/Categories';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
@@ -14,8 +12,8 @@ const Home = () => {
 
   const initialState = useInitialState(API);
   return initialState.length === 0 ? <h1>Loading...</h1> : (
-    <div className='App'>
-      <Header />
+    <>
+
       <Search />
 
       {initialState.mylist.length > 0 && (
@@ -38,8 +36,7 @@ const Home = () => {
         </Carousel>
       </Categories>
 
-      <Footer />
-    </div>
+    </>
   );
 };
 
